e2bc1a2b9938f8b0761e95e8a2a701d001585eef
juan18a <jcda18a@gmail.com>
2026-03-30 00:11:36 -0400

    colocando recuadro faltante en el panel de control para el vendedor

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -55,6 +55,15 @@ export default function Dashboard() {
                         </Link>
                     )}
 
+                    {!isAdmin && (
+
+                        <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                            <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                        </div>
+
+                    )}
+
+
                     <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                         <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
                     </div>
