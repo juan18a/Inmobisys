38eec6acae2e1ee6db2e5d775065bf18534aad9e
juan18a <jcda18a@gmail.com>
2026-03-29 21:52:17 -0400

    ocultar autoeliminacion de usuario

--- resources/js/pages/settings/profile.tsx
+++ resources/js/pages/settings/profile.tsx
@@ -148,7 +148,8 @@ export default function Profile({
                     </Form>
                 </div>
 
-                {!isAdmin && <DeleteUser />}
+
+                {/*  {!isAdmin && <DeleteUser /> */}
             </SettingsLayout>
         </AppLayout>
     );
